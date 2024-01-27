7270da496a809b1c4bea4692357fac699e1ab450
Abhishekgth <128219073+Abhishekgth@users.noreply.github.com>
2024-01-27 10:01:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
- const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+ const band = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
     function stripPrefix(article) {
       const prefixes = ['the ', 'a ', 'an '];
